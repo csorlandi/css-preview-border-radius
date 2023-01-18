08591a02b6845976fd329890e63fa5852b876d68
Claudio Orlandi <claudiosorlandi@gmail.com>
2023-01-17 23:53:53 -0300

    feat(previwer): implement page css previewer section

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,37 @@ function App() {
         <h1>[Previewer]</h1>
         <h2>border-radius</h2>
       </header>
+
+      <section>
+        <span>border-radius:</span>
+        <span>0</span>
+        <span>0</span>
+        <span>0</span>
+        <span>0;</span>
+      </section>
+
+      <main>
+        <div className="row">
+          <label htmlFor="top-left-border">
+            Top Left
+            <input type="number" placeholder="0" id="top-left-border" min={0} step={1} />
+          </label>
+          <label htmlFor="top-right-border">
+            Top Right
+            <input type="number" placeholder="0" id="top-right-border" min={0} step={1} />
+          </label>
+        </div>
+        <div className="border-previewer" />
+        <div className="row">
+          <label htmlFor="bottom-left-border">
+            Bottom Left <input type="number" placeholder="0" id="bottom-left-border" min={0} step={1} />
+          </label>
+          <label htmlFor="bottom-right-border">
+            Bottom Right
+            <input type="number" placeholder="0" id="bottom-right-border" min={0} step={1} />
+          </label>
+        </div>
+      </main>
     </div>
   );
 }

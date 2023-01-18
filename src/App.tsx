e853a09cd9953455175d94a96ed5d77a0d3ba48e
Claudio Orlandi <claudiosorlandi@gmail.com>
2023-01-18 00:45:13 -0300

    feat(feature): implement controlled components to update the previewer border radius

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,29 @@
+import { ChangeEvent, useRef, useState } from 'react';
 import GithubLink from './components/GithubLink';
 
 import './App.css';
 
 function App() {
+  const [radiusValues, setRadiusValues] = useState({
+    'top-left-radius': 50,
+    'top-right-radius': 0,
+    'bottom-left-radius': 0,
+    'bottom-right-radius': 0,
+  });
+
+  const borderPreviewerRef = useRef<HTMLDivElement>(null);
+
+  function handleInputChange(event: ChangeEvent<HTMLInputElement>) {
+    const { id, value, max } = event.target;
+
+    if (Number(value) > Number(max)) return;
+
+    setRadiusValues({
+      ...radiusValues,
+      [id]: Number(value),
+    });
+  }
+
   return (
     <div className="App">
       <GithubLink />
@@ -14,31 +35,90 @@ function App() {
 
       <section>
         <span>border-radius:</span>
-        <span>0</span>
-        <span>0</span>
-        <span>0</span>
-        <span>0;</span>
+        <span>
+          {radiusValues['top-left-radius'] > 0
+            ? `${radiusValues['top-left-radius']}px`
+            : radiusValues['top-left-radius']}
+        </span>
+        <span>
+          {radiusValues['top-right-radius'] > 0
+            ? `${radiusValues['top-right-radius']}px`
+            : radiusValues['top-right-radius']}
+        </span>
+        <span>
+          {radiusValues['bottom-left-radius'] > 0
+            ? `${radiusValues['bottom-left-radius']}px`
+            : radiusValues['bottom-left-radius']}
+        </span>
+        <span>
+          {radiusValues['bottom-right-radius'] > 0
+            ? `${radiusValues['bottom-right-radius']}px`
+            : radiusValues['bottom-right-radius']}
+          ;
+        </span>
       </section>
 
       <main>
         <div className="row">
-          <label htmlFor="top-left-border">
+          <label htmlFor="top-left-radius">
             Top Left
-            <input type="number" placeholder="0" id="top-left-border" min={0} step={1} />
+            <input
+              type="number"
+              value={radiusValues['top-left-radius']}
+              onChange={handleInputChange}
+              id="top-left-radius"
+              min={0}
+              max={999}
+              step={1}
+            />
           </label>
-          <label htmlFor="top-right-border">
+          <label htmlFor="top-right-radius">
             Top Right
-            <input type="number" placeholder="0" id="top-right-border" min={0} step={1} />
+            <input
+              type="number"
+              value={radiusValues['top-right-radius']}
+              onChange={handleInputChange}
+              id="top-right-radius"
+              min={0}
+              max={999}
+              step={1}
+            />
           </label>
         </div>
-        <div className="border-previewer" />
+        <div
+          ref={borderPreviewerRef}
+          className="border-previewer"
+          style={{
+            borderTopLeftRadius: radiusValues['top-left-radius'],
+            borderTopRightRadius: radiusValues['top-right-radius'],
+            borderBottomLeftRadius: radiusValues['bottom-left-radius'],
+            borderBottomRightRadius: radiusValues['bottom-right-radius'],
+          }}
+        />
         <div className="row">
-          <label htmlFor="bottom-left-border">
-            Bottom Left <input type="number" placeholder="0" id="bottom-left-border" min={0} step={1} />
+          <label htmlFor="bottom-left-radius">
+            Bottom Left
+            <input
+              type="number"
+              value={radiusValues['bottom-left-radius']}
+              onChange={handleInputChange}
+              id="bottom-left-radius"
+              min={0}
+              max={999}
+              step={1}
+            />
           </label>
-          <label htmlFor="bottom-right-border">
+          <label htmlFor="bottom-right-radius">
             Bottom Right
-            <input type="number" placeholder="0" id="bottom-right-border" min={0} step={1} />
+            <input
+              type="number"
+              value={radiusValues['bottom-right-radius']}
+              onChange={handleInputChange}
+              id="bottom-right-radius"
+              min={0}
+              max={999}
+              step={1}
+            />
           </label>
         </div>
       </main>
